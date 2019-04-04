def68177265962d44506cf053924bd8178ada917
ray <raykrishardi@gmail.com>
2019-04-04 21:46:30 +1100

    Initial design and UIPickerView setup

--- FibbageSwift/ViewController.swift
+++ FibbageSwift/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerPicker: UIPickerView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Class-level variable
+    var answers = ["a", "b", "c", "d"]
+    
+    // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        answerPicker.dataSource = self
+        answerPicker.delegate = self
     }
-
+    
+    // MARK: - UIPickerViewDataSource and UIPickerViewDelegate delegate method
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return answers.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return answers[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedAnswer = answers[row]
+        print(selectedAnswer)
+    }
+    
+    // MARK: - IBAction
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
 
 }
 
